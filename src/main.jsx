c232cd690447013708b7dee99e031cb699b88183
sanziana <sanzianagrecu@gmail.com>
2026-04-29 12:01:05 +0200

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -11,5 +12,6 @@ gsap.registerPlugin(useGSAP);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
